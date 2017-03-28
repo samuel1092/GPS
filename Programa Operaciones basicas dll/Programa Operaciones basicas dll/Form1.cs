732628254b41382bccdcc9b23458eec84417e9e2
samuel1092 <samuel.11070039@itsmotul.edu.mx>
2017-03-27 18:06:46 -0600

    DLL - Ejemplo Operaciones Basicas
    Programa en c# que realiza la suma y multiplicacion con un archivo dll

---
+++ Programa Operaciones basicas dll/Programa Operaciones basicas dll/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibraryHerramientas;
+
+namespace Programa_Operaciones_basicas_dll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            operaciones op = new operaciones();
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            String respuestas = textBox3.Text + "" + op.suma(a, b);
+            textBox3.Text = respuestas;
+
+            String resmul = textBox4.Text + " " + op.multiplica(a, b);
+           textBox4.Text = resmul;
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text= "";
+            textBox4.Text = "";
+        }
+    }
+}
