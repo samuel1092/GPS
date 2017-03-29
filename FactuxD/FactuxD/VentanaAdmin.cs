20446842ada6e3f71b4ce254443e8bbe8eaff793
samuel1092 <samuel.11070039@itsmotul.edu.mx>
2017-03-28 18:25:55 -0600

    Actualizacion del sistema FACDX
    Creacion de multiples pantallas

---
+++ FactuxD/FactuxD/VentanaAdmin.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiLibrary;
+
+namespace FactuxD
+{
+    public partial class VentanaAdmin : FormBase
+    {
+        public VentanaAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void VentanaAdmin_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void VentanaAdmin_Load(object sender, EventArgs e)
+        {
+            string cmd = "SELECT * FROM Usuarios WHERE id_usuario=" + VentanaLogin.Codigo;
+
+            DataSet DS = Utilidades.Ejecutar(cmd);
+
+
+            lblNomAdmin.Text = DS.Tables[0].Rows[0]["Nom_usua"].ToString();
+            lblUsAdmin.Text = DS.Tables[0].Rows[0]["account"].ToString();
+            lblCodigo.Text = DS.Tables[0].Rows[0]["id_usuario"].ToString();
+
+
+
+            string url = DS.Tables[0].Rows[0]["Foto"].ToString();
+
+            pictureBox1.Image = Image.FromFile(url);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ContenedorPrincipal ContP = new ContenedorPrincipal();
+            this.Hide();
+            ContP.Show();
+        }
+    }
+}
---
+++ FactuxD/FactuxD/VentanaUser.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MiLibrary;
+
+namespace FactuxD
+{
+    public partial class VentanaUser : FormBase
+    {
+        public VentanaUser()
+        {
+            InitializeComponent();
+        }
+
+        private void VentanaUser_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void VentanaUser_Load(object sender, EventArgs e)
+        {
+
+            string cmd = "SELECT * FROM Usuarios WHERE id_usuario=" + VentanaLogin.Codigo;
+
+            DataSet DS = Utilidades.Ejecutar(cmd);
+
+
+            lblNomUs.Text = DS.Tables[0].Rows[0]["Nom_usua"].ToString();
+            lblUs.Text = DS.Tables[0].Rows[0]["account"].ToString();
+            lblCodigo.Text = DS.Tables[0].Rows[0]["id_usuario"].ToString();
+
+
+
+            string url = DS.Tables[0].Rows[0]["Foto"].ToString();
+
+            pictureBox1.Image = Image.FromFile(url);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ContenedorPrincipal ContP = new ContenedorPrincipal();
+            this.Hide();
+            ContP.Show();
+        }
+    }
+}
