5e335e773833bca46a071aa69d69062fd078a0b6
samuel1092 <samuel.11070039@itsmotul.edu.mx>
2017-03-31 18:31:27 -0600

    Creacion Pantallas Sucursales
    Se realizo la creación de las pantallas para gestionar sucursales

---
+++ GPS-Boxito/GPS-Boxito/Controllers/SucursalsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GPS_Boxito.Models;
+
+namespace GPS_Boxito.Controllers
+{
+    public class SucursalsController : Controller
+    {
+        private GPS_BoxitoContext db = new GPS_BoxitoContext();
+
+        // GET: Sucursals
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Sucursals.ToListAsync());
+        }
+
+        // GET: Sucursals/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = await db.Sucursals.FindAsync(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // GET: Sucursals/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Sucursals/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id_sucursal,nombre,region,reparto,latitud,longitud")] Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Sucursals.Add(sucursal);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(sucursal);
+        }
+
+        // GET: Sucursals/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = await db.Sucursals.FindAsync(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: Sucursals/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id_sucursal,nombre,region,reparto,latitud,longitud")] Sucursal sucursal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sucursal).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(sucursal);
+        }
+
+        // GET: Sucursals/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Sucursal sucursal = await db.Sucursals.FindAsync(id);
+            if (sucursal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sucursal);
+        }
+
+        // POST: Sucursals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Sucursal sucursal = await db.Sucursals.FindAsync(id);
+            db.Sucursals.Remove(sucursal);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
